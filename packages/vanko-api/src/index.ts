d2a8cd6de310dbda61e430bb2c747ffbb7e6f07b
Marcos Brito <marcos.brito0507@gmail.com>
2024-08-12 23:09:16 -0300

    feat: setup logging

--- packages/vanko-api/src/index.ts
+++ packages/vanko-api/src/index.ts
@@ -1,7 +1,14 @@
 import Koa from "koa";
 import router from "@/routes/router.ts";
+import logger from "@/logger.ts";
 
 const app = new Koa();
 
+app.on("error", (err) => {
+    logger.error(err);
+});
+
 app.use(router.routes());
 app.listen("3000");
+
+logger.info(`Server listening at 3000`);
