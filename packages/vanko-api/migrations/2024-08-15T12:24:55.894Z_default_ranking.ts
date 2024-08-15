02688993d8a180afdaa50a816a5ca2370366d4bb
Marcos Brito <marcos.brito0507@gmail.com>
2024-08-15 16:49:24 -0300

    fix: add missing execute in migration file

--- packages/vanko-api/migrations/2024-08-15T12:24:55.894Z_default_ranking.ts
+++ packages/vanko-api/migrations/2024-08-15T12:24:55.894Z_default_ranking.ts
@@ -3,11 +3,13 @@ import type { Kysely } from "kysely";
 export async function up(db: Kysely<any>): Promise<void> {
     db.schema
         .alterTable("user")
-        .alterColumn("ranking", (ac) => ac.setDefault("1"));
+        .alterColumn("ranking", (ac) => ac.setDefault("1"))
+        .execute();
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
     db.schema
         .alterTable("user")
-        .alterColumn("ranking", (ac) => ac.dropDefault());
+        .alterColumn("ranking", (ac) => ac.dropDefault())
+        .execute();
 }
