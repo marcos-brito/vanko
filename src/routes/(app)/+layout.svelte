0c56931732ac30350d972a2264572763c8b8af17
Marcos Brito <marcos.brito0507@gmail.com>
2024-09-08 18:52:15 -0300

    feat: create root layout

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+    import { UserIcon, ShoppingCartIcon, SearchIcon } from "lucide-svelte";
+    import type { LayoutData } from "./$types";
+    import * as Tabs from "$lib/components/ui/tabs/index.js";
+    import SignupForm from "$lib/components/signup-form.svelte";
+    import SigninForm from "$lib/components/signin-form.svelte";
+    import * as Sheet from "$lib/components/ui/sheet/index.js";
+
+    export let data: LayoutData;
+</script>
+
+<div class="p-8">
+    <nav class="flex gap-16 justify-end items-center mb-16">
+        <h1 class="text-3xl font-semibold flex-grow">vanko</h1>
+        <div class="flex-grow flex gap-5">
+            <a href="/"> Início </a>
+            <a href="/shop"> Loja </a>
+        </div>
+        <div class="flex gap-5">
+            <SearchIcon />
+            <a href="/cart">
+                <ShoppingCartIcon />
+            </a>
+            {#if data.session}
+                <a href="/account">
+                    <UserIcon />
+                </a>
+            {:else}
+                <Sheet.Root>
+                    <Sheet.Trigger>
+                        <UserIcon />
+                    </Sheet.Trigger>
+                    <Sheet.Content side="right" class="flex flex-col gap-4">
+                        <Sheet.Header>
+                            <Sheet.Title>Use sua conta</Sheet.Title>
+                            <Sheet.Description>
+                                Use sua conta para acessar ofertas exclusivas,
+                                acompanhar pedidos e muito mais. É rápido e
+                                fácil!
+                            </Sheet.Description>
+                        </Sheet.Header>
+                        <Tabs.Root value="signin">
+                            <Tabs.List class="grid grid-cols-2 mb-4">
+                                <Tabs.Trigger value="signin"
+                                    >Já tenho uma conta</Tabs.Trigger
+                                >
+                                <Tabs.Trigger value="signup"
+                                    >Não tenho uma conta</Tabs.Trigger
+                                >
+                            </Tabs.List>
+                            <Tabs.Content value="signup">
+                                <SignupForm
+                                    data={data.form.signup}
+                                    class="flex flex-col gap-4"
+                                />
+                            </Tabs.Content>
+                            <Tabs.Content value="signin">
+                                <SigninForm
+                                    data={data.form.signin}
+                                    class="flex flex-col gap-4"
+                                />
+                            </Tabs.Content>
+                        </Tabs.Root>
+                    </Sheet.Content>
+                </Sheet.Root>
+            {/if}
+        </div>
+    </nav>
+    <slot />
+</div>
