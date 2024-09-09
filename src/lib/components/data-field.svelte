2c9c732e825869fb991eac0c16c64a55d125e991
Marcos Brito <marcos.brito0507@gmail.com>
2024-09-09 02:43:04 -0300

    fixup! feat: allow user to edit information

---
+++ src/lib/components/data-field.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    export let label: string;
+    export let value: string | Date | undefined;
+
+    if (value instanceof Date) {
+        const options = { year: "numeric", month: "numeric", day: "numeric" };
+
+        value = value.toLocaleDateString("pt-BR", options);
+    }
+</script>
+
+<div class="flex flex-col gap-3">
+    <p class="opacity-70 first-letter:capitalize">{label}</p>
+    <p>{value ? value : "---"}</p>
+</div>
