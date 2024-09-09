3dbd295d4f727f28493a0d85b59bee1190ca7bc3
Marcos Brito <marcos.brito0507@gmail.com>
2024-09-09 15:58:37 -0300

    refactor: move account page to info group

---
+++ src/routes/(app)/(info)/+layout.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import SideBar from "$lib/components/side-bar.svelte";
+    import { UserIcon, MapPinIcon, BoxIcon } from "lucide-svelte";
+
+    const routes = [
+        {
+            target: "/account",
+            alias: "Conta",
+            icon: UserIcon
+        },
+        {
+            target: "/orders",
+            alias: "Pedidos",
+            icon: BoxIcon
+        },
+        {
+            target: "/addresses",
+            alias: "Endereços",
+            icon: MapPinIcon
+        }
+    ];
+</script>
+
+<section class="flex flex-col md:flex-row">
+    <SideBar {routes} />
+    <slot />
+</section>
