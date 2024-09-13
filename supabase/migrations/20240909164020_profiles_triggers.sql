e9080c8d59f86f6486a9f7f40b901c46571040a1
Marcos Brito <marcos.brito0507@gmail.com>
2024-09-12 23:22:04 -0300

    fix: add quotes in supabase migrations

--- supabase/migrations/20240909164020_profiles_triggers.sql
+++ supabase/migrations/20240909164020_profiles_triggers.sql
@@ -1,47 +1,51 @@
-CREATE FUNCTION public.handle_new_user () RETURNS TRIGGER LANGUAGE plpgsql SECURITY DEFINER
+CREATE FUNCTION "public"."handle_new_user" () RETURNS TRIGGER LANGUAGE plpgsql SECURITY DEFINER
 SET
     search_path = '' AS $$
 BEGIN
-  INSERT INTO public.profiles (
-    id,
-    name
+  INSERT INTO "public"."profiles" (
+    "id",
+    "name"
   )
   VALUES (
-    new.id,
-    new.raw_user_meta_data ->> 'name'
+    NEW."id",
+    NEW."raw_user_meta_data" ->> 'name'
   );
-  RETURN new;
+  RETURN NEW;
 END;
 $$;
 
-CREATE TRIGGER on_auth_user_created
-AFTER INSERT ON auth.users FOR EACH ROW
-EXECUTE PROCEDURE public.handle_new_user ();
+CREATE TRIGGER "on_auth_user_created"
+AFTER INSERT ON "auth"."users" FOR EACH ROW
+EXECUTE PROCEDURE "public"."handle_new_user" ();
 
 CREATE
-OR REPLACE FUNCTION public.handle_user_update () RETURNS TRIGGER LANGUAGE plpgsql SECURITY DEFINER
+OR REPLACE FUNCTION "public"."handle_user_update" () RETURNS TRIGGER LANGUAGE plpgsql SECURITY DEFINER
 SET
     search_path = '' AS $$
 BEGIN
-  UPDATE public.profiles
+  UPDATE "public"."profiles"
   SET
-    name = new.raw_user_meta_data ->> 'name',
-    email = new.raw_user_meta_data ->> 'email',
-    cpf = new.raw_user_meta_data ->> 'cpf',
-    phone = new.raw_user_meta_data ->> 'phone',
-    gender = COALESCE(NULLIF(new.raw_user_meta_data ->> 'gender', '')::public.gender, NULL),
-    birth = COALESCE(
-                    NULLIF(
-                      NULLIF(NEW.raw_user_meta_data ->> 'birth', ''),
-                      'null'
-                    )::date,
-                    NULL
-                  )  WHERE id = new.id;
-  RETURN new;
+    "name" = NEW."raw_user_meta_data" ->> 'name',
+    "email" = NEW."raw_user_meta_data" ->> 'email',
+    "cpf" = NEW."raw_user_meta_data" ->> 'cpf',
+    "phone" = NEW."raw_user_meta_data" ->> 'phone',
+    "gender" = COALESCE(
+      NULLIF(NEW."raw_user_meta_data" ->> 'gender', '')::"public"."gender", 
+      NULL
+    ),
+    "birth" = COALESCE(
+      NULLIF(
+        NULLIF(NEW."raw_user_meta_data" ->> 'birth', ''), 
+        'null'
+      )::DATE, 
+      NULL
+    )  
+  WHERE "id" = NEW."id";
+  RETURN NEW;
 END;
 $$;
 
-CREATE TRIGGER on_auth_user_updated
+CREATE TRIGGER "on_auth_user_updated"
 AFTER
-UPDATE ON auth.users FOR EACH ROW
-EXECUTE FUNCTION public.handle_user_update ();
+UPDATE ON "auth"."users" FOR EACH ROW
+EXECUTE FUNCTION "public"."handle_user_update" ();
--- supabase/migrations/20240909164216_verify_password.sql
+++ supabase/migrations/20240909164216_verify_password.sql
@@ -1,18 +1,18 @@
 CREATE
-OR REPLACE FUNCTION public.verify_user_password (user_id TEXT, password TEXT) RETURNS BOOLEAN LANGUAGE plpgsql SECURITY DEFINER
+OR REPLACE FUNCTION "public"."verify_user_password" ("user_id" TEXT, "password" TEXT) RETURNS BOOLEAN LANGUAGE plpgsql SECURITY DEFINER
 SET
-    search_path = extensions,
-    public,
-    auth AS $$
+    search_path = "extensions",
+    "public",
+    "auth" AS $$
 BEGIN
-    ASSERT user_id IS NOT NULL, 'user_id should not be null';
-    ASSERT password IS NOT NULL, 'password should not be null';
+    ASSERT "user_id" IS NOT NULL, 'user_id should not be null';
+    ASSERT "password" IS NOT NULL, 'password should not be null';
 
   RETURN EXISTS (
-    SELECT id
-    FROM auth.users
-    WHERE id = user_id::uuid
-      AND encrypted_password = crypt(password, encrypted_password)
+    SELECT "id"
+    FROM "auth"."users"
+    WHERE "id" = "user_id"::UUID
+      AND "encrypted_password" = crypt("password", "encrypted_password")
   );
 END;
-$$
+$$;
