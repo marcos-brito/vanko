26acacf9fcc2cb47d9f8c5323575f1962bf92c35
Marcos Brito <marcos.brito0507@gmail.com>
2025-03-10 13:06:29 -0300

    chore: update shadcn-svelte

--- src/lib/components/ui/form/form-field-errors.svelte
+++ src/lib/components/ui/form/form-field-errors.svelte
@@ -1,35 +1,30 @@
 <script lang="ts">
 	import * as FormPrimitive from "formsnap";
+	import type { WithoutChild } from "bits-ui";
 	import { cn } from "$lib/utils.js";
 
-	type $$Props = FormPrimitive.FieldErrorsProps & {
+	let {
+		ref = $bindable(null),
+		class: className,
+		errorClasses,
+		children: childrenProp,
+		...restProps
+	}: WithoutChild<FormPrimitive.FieldErrorsProps> & {
 		errorClasses?: string | undefined | null;
-	};
-
-	
-	interface Props {
-		class?: $$Props["class"];
-		errorClasses?: $$Props["class"];
-		children?: import('svelte').Snippet<[any]>;
-		[key: string]: any
-	}
-
-	let { class: className = undefined, errorClasses = undefined, children, ...rest }: Props = $props();
-
-	const children_render = $derived(children);
+	} = $props();
 </script>
 
 <FormPrimitive.FieldErrors
+	bind:ref
 	class={cn("text-destructive text-sm font-medium", className)}
-	{...rest}
-	
-	
-	
+	{...restProps}
 >
-	{#snippet children({ errors, fieldErrorsAttrs, errorAttrs })}
-		{#if children_render}{@render children_render({ errors, fieldErrorsAttrs, errorAttrs, })}{:else}
+	{#snippet children({ errors, errorProps })}
+		{#if childrenProp}
+			{@render childrenProp({ errors, errorProps })}
+		{:else}
 			{#each errors as error}
-				<div {...errorAttrs} class={cn(errorClasses)}>{error}</div>
+				<div {...errorProps} class={cn(errorClasses)}>{error}</div>
 			{/each}
 		{/if}
 	{/snippet}
