d507b247cdddef55552649a114caca0f0345ab42
Marcos Brito <marcos.brito0507@gmail.com>
2025-03-10 14:57:53 -0300

    fix: use the right size for cep

--- drizzle/0004_fix cep length.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE "order_address";--> statement-breakpoint
-DROP TABLE "order_products";--> statement-breakpoint
-DROP TABLE "orders";--> statement-breakpoint
-ALTER TABLE "addresses" ALTER COLUMN "zip_code" SET DATA TYPE char(8);
