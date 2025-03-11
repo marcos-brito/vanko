3fe2256ffac6942c700f426a59a60b9dd6b62e67
Marcos Brito <marcos.brito0507@gmail.com>
2025-03-10 22:27:20 -0300

    refactor: make better functions to handle form errors

--- src/lib/error.ts
+++ src/lib/error.ts
@@ -1,21 +1,41 @@
+import { toast } from "svelte-sonner";
 import { message, type SuperValidated } from "sveltekit-superforms";
 
-export const invalidFormMessage =
+export const errInvalid =
     "Haviam alguns dados inválidos no formulário. Corrija e tente novamente";
 
-export const genericError =
+export const errGeneric =
     "Tivemos alguns problemas acessando seus dados. Tente novamente mais tarde";
 
-export function actionError(form: SuperValidated<any>, msg: string) {
+export function err(form: SuperValidated<any>, msg: string) {
     return message(form, {
         type: "error",
         text: msg
     });
 }
 
-export function actionSuccess(form: SuperValidated<any>, msg: string) {
+export function success(form: SuperValidated<any>, msg: string) {
     return message(form, {
         type: "success",
         text: msg
     });
+}
+
+export function maybeShowMessage(form: SuperValidated<any>): void {
+    if (form.message) showMessage(form.message);
+    if (!form.valid) showMessage({ type: "error", text: errInvalid });
+}
+
+export function showMessage(message: App.Superforms.Message) {
+    switch (message.type) {
+        case "success":
+            toast.success("Sucesso", { description: message.text });
+            break;
+        case "error":
+            toast.error("Erro", { description: message.text });
+            break;
+        case "warning":
+            toast.warning("Aviso", { description: message.text });
+            break;
+    }
 }
