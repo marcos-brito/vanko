5f2c67b1b47a56dbc95a50d4aa33ea5efc792e35
Marcos Brito <marcos.brito0507@gmail.com>
2025-03-18 15:58:00 -0300

    chore: update some components

---
+++ src/lib/components/checkbox.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import { Label } from "$lib/components/ui/label/index.js";
+    import { Checkbox } from "$lib/components/ui/checkbox";
+
+    interface Props {
+        label: string;
+        checked: boolean;
+    }
+
+    let { label, checked = $bindable() }: Props = $props();
+</script>
+
+<div class="flex gap-2 items-center">
+    <Checkbox id={label} aria-labelledby={`${label}-label`} bind:checked />
+    <Label id={`${label}-label`} for={label} class="text-sm">
+        {label}
+    </Label>
+</div>
