c1d13b426df45f5184ed8a3f43ff8f6d23fe5978
Marcos Brito <marcos.brito0507@gmail.com>
2025-03-18 12:29:30 -0300

    fix: change drop order in migration

--- drizzle/0006_denomalize_address.sql
+++ drizzle/0006_denomalize_address.sql
@@ -12,8 +12,6 @@ ALTER TYPE "order_status" ADD VALUE 'troca solicitada';--> statement-breakpoint
 ALTER TYPE "order_status" ADD VALUE 'em troca';--> statement-breakpoint
 ALTER TYPE "order_status" ADD VALUE 'troca recebida';--> statement-breakpoint
 ALTER TYPE "order_status" ADD VALUE 'troca finalizada';--> statement-breakpoint
-DROP TABLE "countries";--> statement-breakpoint
-DROP TABLE "states";--> statement-breakpoint
 ALTER TABLE "addresses" DROP CONSTRAINT "addresses_country_countries_id_fk";
 --> statement-breakpoint
 ALTER TABLE "addresses" DROP CONSTRAINT "addresses_state_states_id_fk";
@@ -22,6 +20,8 @@ ALTER TABLE "order_address" DROP CONSTRAINT "order_address_country_countries_id_
 --> statement-breakpoint
 ALTER TABLE "order_address" DROP CONSTRAINT "order_address_state_states_id_fk";
 --> statement-breakpoint
+DROP TABLE "countries";--> statement-breakpoint
+DROP TABLE "states";--> statement-breakpoint
 ALTER TABLE "addresses" ALTER COLUMN "state" SET DATA TYPE char(2);--> statement-breakpoint
 ALTER TABLE "profiles" ALTER COLUMN "status" SET DEFAULT 'ativo';--> statement-breakpoint
 ALTER TABLE "order_address" ALTER COLUMN "state" SET DATA TYPE char(2);--> statement-breakpoint
