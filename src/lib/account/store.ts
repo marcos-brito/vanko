531e89cbf4bb095252325559310f118edfce93ab
Marcos Brito <marcos.brito0507@gmail.com>
2025-03-25 12:36:04 -0300

    chore: remove count function

--- src/lib/account/store.ts
+++ src/lib/account/store.ts
@@ -1,6 +1,6 @@
 import { db } from "$lib/db";
 import { profiles, type SelectProfile } from "$lib/models";
-import { eq, count } from "drizzle-orm";
+import { eq } from "drizzle-orm";
 
 export async function findUserProfile(
     uuid: string
@@ -10,18 +10,25 @@ export async function findUserProfile(
     });
 }
 
-export async function profileIsMissingInfo(uuid: string): Promise<boolean> {
-    const fields: Array<keyof SelectProfile> = ["name", "email", "cpf", "phone", "gender", "birth"];
-
-    return await findUserProfile(uuid).then((profile) => {
-        return fields.some((field) => profile && (profile[field] == undefined || profile[field] == null))
-    })
-}
-
 export async function findProfiles(): Promise<Array<SelectProfile>> {
     return await db.query.profiles.findMany();
 }
 
-export async function countProfiles(): Promise<number | undefined> {
-    return await db.select({ count: count() }).from(profiles).then((result) => result.at(0)?.count);
+export async function profileIsMissingInfo(uuid: string): Promise<boolean> {
+    const fields: Array<keyof SelectProfile> = [
+        "name",
+        "email",
+        "cpf",
+        "phone",
+        "gender",
+        "birth"
+    ];
+
+    return await findUserProfile(uuid).then((profile) => {
+        return fields.some(
+            (field) =>
+                profile &&
+                (profile[field] == undefined || profile[field] == null)
+        );
+    });
 }
