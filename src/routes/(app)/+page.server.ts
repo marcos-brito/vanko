4a95d6b320c335930309cb77a3685abb51de4264
Marcos Brito <marcos.brito0507@gmail.com>
2025-03-25 12:38:13 -0300

    fix: use error functions for auth endpoints

--- src/routes/(app)/+page.server.ts
+++ src/routes/(app)/+page.server.ts
@@ -1,13 +1,13 @@
 import { signinSchema, signupSchema } from "$lib/auth/schema";
-import { invalidFormMessage } from "$lib/utils";
+import { err, errInvalid } from "$lib/error";
 import { redirect, type Actions } from "@sveltejs/kit";
-import { message, superValidate } from "sveltekit-superforms";
+import { superValidate } from "sveltekit-superforms";
 import { zod } from "sveltekit-superforms/adapters";
 
 export const actions: Actions = {
     signup: async ({ request, locals: { supabase } }) => {
         const form = await superValidate(request, zod(signupSchema));
-        if (!form.valid) return message(form, invalidFormMessage);
+        if (!form.valid) return err(form, errInvalid);
 
         const { error } = await supabase.auth.signUp({
             email: form.data.email,
@@ -19,30 +19,24 @@ export const actions: Actions = {
             }
         });
 
-        if (error) {
-            return message(form, {
-                type: "error",
-                text: "Não conseguimos criar sua conta. Tente novamente mais tarde."
-            });
-        }
+        if (error)
+            err(
+                form,
+                "Não conseguimos criar sua conta. Tente novamente mais tarde."
+            );
 
         return redirect(303, "/");
     },
     signin: async ({ request, locals: { supabase } }) => {
         const form = await superValidate(request, zod(signinSchema));
-        if (!form.valid) return message(form, invalidFormMessage);
+        if (!form.valid) return err(form, errInvalid);
 
         const { error } = await supabase.auth.signInWithPassword({
             email: form.data.email,
             password: form.data.password
         });
 
-        if (error) {
-            return message(form, {
-                type: "error",
-                text: "Email ou senha inválidos"
-            });
-        }
+        if (error) err(form, "Email ou senha inválidos");
 
         redirect(303, "/");
     }
